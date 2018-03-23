58c2bce4f31dd6af6169deaa7ca585f1fdd74567
Cloud User <centos@ip-172-31-82-17.ec2.internal>
2018-03-23 00:24:36 +0000

    using the chef template

--- recipes/server.rb
+++ recipes/server.rb
@@ -8,10 +8,14 @@ package 'httpd' do
   action :install
 end
 
-file '/var/www/html/index.html' do
-  content "<h1>Hello, World!</h1>
-  ipaddress: #{ node['ipaddress']}
-  hostname : #{ node['hostname']}"
+#file '/var/www/html/index.html' do
+#  content "<h1>Hello, World!</h1>
+#  ipaddress: #{ node['ipaddress']}
+#  hostname : #{ node['hostname']}"
+#end
+
+template '/var/www/html/index.html' do
+  source 'index.html.erb'
 end
 
 service 'httpd' do
